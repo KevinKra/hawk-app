413f50974da8dd12f00de29d045e873ea9776a3f
KevinKra <kratok08@gmail.com>
2022-02-25 20:47:39 -0700

    initial commit

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const checkAPI = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/hawk/list");
+        console.log({ response });
+      } catch (error) {
+        console.log({ error });
+      }
+    };
+    checkAPI();
+  }, []);
+
   return (
     <div className="App">
       <h1>hello world</h1>
     </div>
   );
-}
+};
 
 export default App;
