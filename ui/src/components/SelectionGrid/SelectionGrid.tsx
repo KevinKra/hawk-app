e57a0a391e16eb50f9e1f231e7be45cb162e376d
KevinKra <kratok08@gmail.com>
2022-02-27 23:16:59 -0700

    light refactor pass

--- ui/src/components/SelectionGrid/SelectionGrid.tsx
+++ ui/src/components/SelectionGrid/SelectionGrid.tsx
@@ -2,11 +2,11 @@ import { Button, Paper, styled, Typography } from "@mui/material";
 import React from "react";
 import { useGetHawkContext } from "../../context";
 
+// todo - resolve build/dev cors error
 // todo - set current selection viewable from both components
 // todo - configure sortable grid + buttons
 // todo - adjust casing methods for column data
-// todo - resolve build/dev cors error
-// todo - any many more things ...
+// todo - and more!
 
 const SelectionGrid = () => {
   const hawks = useGetHawkContext();
@@ -91,6 +91,10 @@ const GridBase = styled("div")`
   grid-gap: 0.5rem;
 `;
 
-const GridHeader = styled(GridBase)``;
+const GridHeader = styled(GridBase)`
+  // ? feature style features here
+`;
 
-const GridRow = styled(GridBase)``;
+const GridRow = styled(GridBase)`
+  // ? feature style features here
+`;
